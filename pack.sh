205d3e9125a06163743fed16ca16583f1afad721
ideawu <wuzuyang@wuzuyang.local>
2013-03-19 10:18:41 +0800

    edit conf teplates and pack.sh

--- pack.sh
+++ pack.sh
@@ -12,18 +12,18 @@ mkdir $dist_dir/var_slave
 touch $dist_dir/var/.gitignore
 touch $dist_dir/var_slave/.gitignore
 
-cp -r api $dist_dir
-cp -r deps $dist_dir
-cp -r src $dist_dir
-cp -r tools $dist_dir
-cp -r pack.sh $dist_dir
-cp -r build.sh $dist_dir
-cp -r README.md $dist_dir
-cp -r TODO $dist_dir
-cp -r version $dist_dir
-cp -r Makefile.dist $dist_dir/Makefile
-cp -r ssdb.conf.dist $dist_dir/ssdb.conf
-cp -r ssdb_slave.conf.dist $dist_dir/ssdb_slave.conf
+cp -R api $dist_dir
+cp -R deps $dist_dir
+cp -R src $dist_dir
+cp -R tools $dist_dir
+cp -R pack.sh $dist_dir
+cp -R build.sh $dist_dir
+cp -R README.md $dist_dir
+cp -R TODO $dist_dir
+cp -R version $dist_dir
+cp -R Makefile.dist $dist_dir/Makefile
+cp -R ssdb.conf.dist $dist_dir/ssdb.conf
+cp -R ssdb_slave.conf.dist $dist_dir/ssdb_slave.conf
 
 cd $dist_dir
 make clean
@@ -32,4 +32,4 @@ cd ..
 tar czf ${dist}.tar.gz $dist
 
 cd "$DIR"
-cp -r $dist_dir/* git/
+
--- src/version.h
+++ src/version.h
@@ -1,3 +1,3 @@
 #ifndef SSDB_VERSION
-#define SSDB_VERSION "1.2.10"
+#define SSDB_VERSION "1.3.0"
 #endif
