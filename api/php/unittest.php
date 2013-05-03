977e22b82345eab6265511f472ba116e94a89d3c
ideawu <wuzuyang@wuzuyang.local>
2013-05-03 17:29:18 +0800

    modify kL0_*Trigger

---
+++ api/php/unittest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Copyright (c) 2012, ideawu
+ * All rights reserved.
+ * @author: ideawu
+ * @link: http://www.ideawu.com/
+ *
+ * SSDB PHP API demo.
+ */
+
+include('SSDB.php');
+$host = '127.0.0.1';
+$port = 8888;
+
+$ssdb = new SimpleSSDB($host, $port);
+
+// TODO:
+
+
+
