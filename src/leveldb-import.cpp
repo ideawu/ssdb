160751deb6b3a20943e0b9d88dd4970e27583576
ideawu <wuzuyang@wuzuyang.local>
2013-08-29 22:42:36 +0800

    update leveldb-import.cpp

--- src/leveldb-import.cpp
+++ src/leveldb-import.cpp
@@ -13,30 +13,6 @@
 #include "util/file.h"
 #include "util/strings.h"
 
-template<class T>
-static std::string serialize_req(T &req){
-	std::string ret;
-	char buf[50];
-	for(int i=0; i<req.size(); i++){
-		if(i >= 5 && i < req.size() - 1){
-			sprintf(buf, "[%d more...]", (int)req.size() - i - 1);
-			ret.append(buf);
-			break;
-		}
-		if(((req[0] == "get" || req[0] == "set") && i == 1) || req[i].size() < 30){
-			std::string h = hexmem(req[i].data(), req[i].size());
-			ret.append(h);
-		}else{
-			sprintf(buf, "[%d bytes]", (int)req[i].size());
-			ret.append(buf);
-		}
-		if(i < req.size() - 1){
-			ret.append(" ");
-		}
-	}
-	return ret;
-}
-
 void welcome(){
 	printf("leveldb-import - Import existing leveldb into ssdb\n");
 	printf("Copyright (c) 2013 ideawu.com\n");
@@ -103,11 +79,6 @@ int main(int argc, char **argv){
 		if(req == NULL){
 			printf("error\n");
 			exit(0);
-		}else if(req->empty()){
-			if(link->read() <= 0){
-				printf("server disconnected.\n");
-				exit(0);
-			}
 		}else{
 			if(req->at(0) != "ok"){
 				printf("server response error: %s\n", req->at(0).String().c_str());
