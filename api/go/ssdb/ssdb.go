3321f7236753edaf79307145d8df03c2584a8acd
Jiang Bian <borderj@gmail.com>
2013-12-11 20:02:25 +0800

    code style by go fmt

--- api/go/ssdb/ssdb.go
+++ api/go/ssdb/ssdb.go
@@ -1,165 +1,164 @@
 package ssdb
 
 import (
-		"fmt"
-		"net"
-		"bytes"
-		"strconv"
-	   )
+	"bytes"
+	"fmt"
+	"net"
+	"strconv"
+)
 
-type Client struct{
-	sock *net.TCPConn
+type Client struct {
+	sock     *net.TCPConn
 	recv_buf bytes.Buffer
 }
 
-func Connect(ip string, port int) (*Client, error){
-	addr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%d", ip, port));
-	if err != nil{
-		return nil, err;
+func Connect(ip string, port int) (*Client, error) {
+	addr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%d", ip, port))
+	if err != nil {
+		return nil, err
 	}
-	sock, err := net.DialTCP("tcp", nil, addr);
-	if err != nil{
-		return nil, err;
+	sock, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
+		return nil, err
 	}
-	var c Client;
-	c.sock = sock;
-	return &c, nil;
+	var c Client
+	c.sock = sock
+	return &c, nil
 }
 
-func (c *Client) Do(args...interface{}) ([]string, error){
-	err := c.send(args);
-	if err != nil{
-		return nil, err;
+func (c *Client) Do(args ...interface{}) ([]string, error) {
+	err := c.send(args)
+	if err != nil {
+		return nil, err
 	}
-	resp, err := c.recv();
-	return resp, err;
+	resp, err := c.recv()
+	return resp, err
 }
 
-func (c *Client) Set(key string, val string) (interface{}, error){
-	resp, err := c.Do("set", key, val);
-	if err != nil{
-		return nil, err;
+func (c *Client) Set(key string, val string) (interface{}, error) {
+	resp, err := c.Do("set", key, val)
+	if err != nil {
+		return nil, err
 	}
-	if len(resp) == 1 && resp[0] == "ok"{
-		return true, nil;
+	if len(resp) == 1 && resp[0] == "ok" {
+		return true, nil
 	}
-	return nil, fmt.Errorf("bad response");
+	return nil, fmt.Errorf("bad response")
 }
 
 // TODO: Will somebody write addition semantic methods?
-func (c *Client) Get(key string) (interface{}, error){
-	resp, err := c.Do("get", key);
-	if err != nil{
-		return nil, err;
+func (c *Client) Get(key string) (interface{}, error) {
+	resp, err := c.Do("get", key)
+	if err != nil {
+		return nil, err
 	}
-	if len(resp) == 2 && resp[0] == "ok"{
-		return resp[1], nil;
+	if len(resp) == 2 && resp[0] == "ok" {
+		return resp[1], nil
 	}
-	if resp[0] == "not_found"{
-		return nil, nil;
+	if resp[0] == "not_found" {
+		return nil, nil
 	}
-	return nil, fmt.Errorf("bad response");
+	return nil, fmt.Errorf("bad response")
 }
 
-func (c *Client) Del(key string) (interface{}, error){
-	resp, err := c.Do("del", key);
-	if err != nil{
-		return nil, err;
+func (c *Client) Del(key string) (interface{}, error) {
+	resp, err := c.Do("del", key)
+	if err != nil {
+		return nil, err
 	}
-	if len(resp) == 1 && resp[0] == "ok"{
-		return true, nil;
+	if len(resp) == 1 && resp[0] == "ok" {
+		return true, nil
 	}
-	return nil, fmt.Errorf("bad response");
+	return nil, fmt.Errorf("bad response")
 }
 
-func (c *Client) send(args []interface{}) error{
-	var buf bytes.Buffer;
-	for _, arg := range args{
-		var s string;
+func (c *Client) send(args []interface{}) error {
+	var buf bytes.Buffer
+	for _, arg := range args {
+		var s string
 		switch arg := arg.(type) {
-			case string:
-				s = arg;
-			case []byte:
-				s = string(arg);
-			case int:
-				s = fmt.Sprintf("%d", arg);
-			case int64:
-				s = fmt.Sprintf("%d", arg);
-			case float64:
-				s = fmt.Sprintf("%f", arg);
-			case bool:
-				if arg {
-					s = "1";
-				} else {
-					s = "0";
-				}
-			case nil:
-				s = "";
-			default:
-				return fmt.Errorf("bad arguments");
+		case string:
+			s = arg
+		case []byte:
+			s = string(arg)
+		case int:
+			s = fmt.Sprintf("%d", arg)
+		case int64:
+			s = fmt.Sprintf("%d", arg)
+		case float64:
+			s = fmt.Sprintf("%f", arg)
+		case bool:
+			if arg {
+				s = "1"
+			} else {
+				s = "0"
+			}
+		case nil:
+			s = ""
+		default:
+			return fmt.Errorf("bad arguments")
 		}
-		buf.WriteString(fmt.Sprintf("%d", len(s)));
-		buf.WriteByte('\n');
-		buf.WriteString(s);
-		buf.WriteByte('\n');
+		buf.WriteString(fmt.Sprintf("%d", len(s)))
+		buf.WriteByte('\n')
+		buf.WriteString(s)
+		buf.WriteByte('\n')
 	}
-	buf.WriteByte('\n');
-	_, err := c.sock.Write(buf.Bytes());
-	return err;
+	buf.WriteByte('\n')
+	_, err := c.sock.Write(buf.Bytes())
+	return err
 }
 
-func (c *Client) recv() ([]string, error){
-	var tmp [8192]byte;
-	for{
-		n, err := c.sock.Read(tmp[0:]);
-		if err != nil{
-			return nil, err;
+func (c *Client) recv() ([]string, error) {
+	var tmp [8192]byte
+	for {
+		n, err := c.sock.Read(tmp[0:])
+		if err != nil {
+			return nil, err
 		}
-		c.recv_buf.Write(tmp[0:n]);
-		resp := c.parse();
-		if resp == nil || len(resp) > 0{
-			return resp, nil;
+		c.recv_buf.Write(tmp[0:n])
+		resp := c.parse()
+		if resp == nil || len(resp) > 0 {
+			return resp, nil
 		}
 	}
 }
 
-func (c *Client) parse() []string{
-	resp := []string{};
-	buf := c.recv_buf.Bytes();
-	var idx, offset int;
-	idx = 0;
-	offset = 0;
+func (c *Client) parse() []string {
+	resp := []string{}
+	buf := c.recv_buf.Bytes()
+	var idx, offset int
+	idx = 0
+	offset = 0
 
-	for{
-		idx = bytes.IndexByte(buf[offset:], '\n');
-		if idx == -1{
-			break;
+	for {
+		idx = bytes.IndexByte(buf[offset:], '\n')
+		if idx == -1 {
+			break
 		}
-		p := buf[offset : offset + idx];
-		offset += idx + 1;
+		p := buf[offset : offset+idx]
+		offset += idx + 1
 		//fmt.Printf("> [%s]\n", p);
-		if len(p)  == 0 || (len(p) == 1 && p[0] == '\r'){
-			if len(resp) == 0{
-				continue;
-			}else{
-				c.recv_buf.Next(offset);
-				return resp;
+		if len(p) == 0 || (len(p) == 1 && p[0] == '\r') {
+			if len(resp) == 0 {
+				continue
+			} else {
+				c.recv_buf.Next(offset)
+				return resp
 			}
 		}
 
-		size, err := strconv.Atoi(string(p));
-		if err != nil || size < 0{
-			return nil;
+		size, err := strconv.Atoi(string(p))
+		if err != nil || size < 0 {
+			return nil
 		}
-		if offset + size >= c.recv_buf.Len(){
-			break;
+		if offset+size >= c.recv_buf.Len() {
+			break
 		}
 
-		v := buf[offset : offset + size];
-		resp = append(resp, string(v));
-		offset += size + 1;
+		v := buf[offset : offset+size]
+		resp = append(resp, string(v))
+		offset += size + 1
 	}
 
-	return []string{};
+	return []string{}
 }
-
