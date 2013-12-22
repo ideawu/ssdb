3d963a417ce2d1f505acb4d266b47cb8925dc8a5
ideawu <ideawu@local>
2013-12-22 14:33:22 +0800

    added .net api

---
+++ api/dotnet/Client.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ssdb
+{
+	public class Client
+	{
+		private Link link;
+		private string resp_code;
+
+		public Client(string host, int port) {
+			link = new Link(host, port);
+		}
+
+		~Client() {
+			this.close();
+		}
+
+		public void close() {
+			link.close();
+		}
+
+		public List<byte[]> request(string cmd, params string[] args) {
+			return link.request(cmd, args);
+		}
+
+		public List<byte[]> request(string cmd, params byte[][] args) {
+			return link.request(cmd, args);
+		}
+
+		public List<byte[]> request(List<byte[]> req) {
+			return link.request(req);
+		}
+
+
+		private void assert_ok() {
+			if(resp_code != "ok") {
+				throw new Exception(resp_code);
+			}
+		}
+
+		private byte[] _bytes(string s) {
+			return Encoding.Default.GetBytes(s);
+		}
+
+		private string _string(byte[] bs) {
+			return Encoding.Default.GetString(bs);
+		}
+
+		private KeyValuePair<string, byte[]>[] parse_scan_resp(List<byte[]> resp) {
+			resp_code = _string(resp[0]);
+			this.assert_ok();
+
+			int size = (resp.Count - 1) / 2;
+			KeyValuePair<string, byte[]>[] kvs = new KeyValuePair<string, byte[]>[size];
+			for(int i = 0; i < size; i += 1) {
+				string key = _string(resp[i * 2 + 1]);
+				byte[] val = resp[i * 2 + 2];
+				kvs[i] = new KeyValuePair<string, byte[]>(key, val);
+			}
+			return kvs;
+		}
+
+		/***** kv *****/
+
+		public void set(byte[] key, byte[] val) {
+			List<byte[]> resp = request("set", key, val);
+			resp_code = _string(resp[0]);
+			this.assert_ok();
+		}
+
+		public void set(string key, string val) {
+			this.set(_bytes(key), _bytes(val));
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="key"></param>
+		/// <param name="val"></param>
+		/// <returns>returns true if name.key is found, otherwise returns false.</returns>
+		public bool get(byte[] key, out byte[] val) {
+			val = null;
+			List<byte[]> resp = request("get", key);
+			resp_code = _string(resp[0]);
+			if(resp_code == "not_found") {
+				return false;
+			}
+			this.assert_ok();
+			if(resp.Count != 2) {
+				throw new Exception("Bad response!");
+			}
+			val = resp[1];
+			return true;
+		}
+
+		public bool get(string key, out byte[] val) {
+			return this.get(_bytes(key), out val);
+		}
+
+		public bool get(string key, out string val) {
+			val = null;
+			byte[] bs;
+			if(!this.get(key, out bs)) {
+				return false;
+			}
+			val = _string(bs);
+			return true;
+		}
+
+		public void del(byte[] key) {
+			List<byte[]> resp = request("del", key);
+			resp_code = _string(resp[0]);
+			this.assert_ok();
+		}
+
+		public void del(string key) {
+			this.del(_bytes(key));
+		}
+
+		public KeyValuePair<string, byte[]>[] scan(string key_start, string key_end, Int64 limit) {
+			List<byte[]> resp = request("scan", key_start, key_end, limit.ToString());
+			return parse_scan_resp(resp);
+		}
+
+		public KeyValuePair<string, byte[]>[] rscan(string key_start, string key_end, Int64 limit) {
+			List<byte[]> resp = request("rscan", key_start, key_end, limit.ToString());
+			return parse_scan_resp(resp);
+		}
+
+		/***** hash *****/
+
+		public void hset(byte[] name, byte[] key, byte[] val) {
+			List<byte[]> resp = request("hset", name, key, val);
+			resp_code = _string(resp[0]);
+			this.assert_ok();
+		}
+
+		public void hset(string name, string key, byte[] val) {
+			this.hset(_bytes(name), _bytes(key), val);
+		}
+
+		public void hset(string name, string key, string val) {
+			this.hset(_bytes(name), _bytes(key), _bytes(val));
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="key"></param>
+		/// <param name="val"></param>
+		/// <returns>returns true if name.key is found, otherwise returns false.</returns>
+		public bool hget(byte[] name, byte[] key, out byte[] val) {
+			val = null;
+			List<byte[]> resp = request("hget", name, key);
+			resp_code = _string(resp[0]);
+			if(resp_code == "not_found") {
+				return false;
+			}
+			this.assert_ok();
+			if(resp.Count != 2) {
+				throw new Exception("Bad response!");
+			}
+			val = resp[1];
+			return true;
+		}
+
+		public bool hget(string name, string key, out byte[] val) {
+			return this.hget(_bytes(name), _bytes(key), out val);
+		}
+
+		public bool hget(string name, string key, out string val) {
+			val = null;
+			byte[] bs;
+			if(!this.hget(name, key, out bs)) {
+				return false;
+			}
+			val = _string(bs);
+			return true;
+		}
+
+		public void hdel(byte[] name, byte[] key) {
+			List<byte[]> resp = request("hdel", name, key);
+			resp_code = _string(resp[0]);
+			this.assert_ok();
+		}
+
+		public void hdel(string name, string key) {
+			this.hdel(_bytes(name), _bytes(key));
+		}
+
+		public Int64 hsize(byte[] name) {
+			List<byte[]> resp = request("hsize", name);
+			resp_code = _string(resp[0]);
+			this.assert_ok();
+			if(resp.Count != 2) {
+				throw new Exception("Bad response!");
+			}
+			return Int64.Parse(_string(resp[1]));
+		}
+
+		public Int64 hsize(string name) {
+			return this.hsize(_bytes(name));
+		}
+
+		public KeyValuePair<string, byte[]>[] hscan(string name, string key_start, string key_end, Int64 limit) {
+			List<byte[]> resp = request("hscan", name, key_start, key_end, limit.ToString());
+			return parse_scan_resp(resp);
+		}
+
+		public KeyValuePair<string, byte[]>[] hrscan(string name, string key_start, string key_end, Int64 limit) {
+			List<byte[]> resp = request("hrscan", name, key_start, key_end, limit.ToString());
+			return parse_scan_resp(resp);
+		}
+
+		/***** zset *****/
+
+		public void zset(byte[] name, byte[] key, Int64 score) {
+			List<byte[]> resp = request("zset", name, key, _bytes(score.ToString()));
+			resp_code = _string(resp[0]);
+			this.assert_ok();
+		}
+		
+		public void zset(string name, string key, Int64 score) {
+			this.zset(_bytes(name), _bytes(key), score);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="key"></param>
+		/// <param name="score"></param>
+		/// <returns>returns true if name.key is found, otherwise returns false.</returns>
+		public bool zget(byte[] name, byte[] key, out Int64 score) {
+			score = -1;
+			List<byte[]> resp = request("zget", name, key);
+			resp_code = _string(resp[0]);
+			if(resp_code == "not_found") {
+				return false;
+			}
+			this.assert_ok();
+			if(resp.Count != 2) {
+				throw new Exception("Bad response!");
+			}
+			score = Int64.Parse(_string(resp[1]));
+			return true;
+		}
+
+		public bool zget(string name, string key, out Int64 score) {
+			return this.zget(_bytes(name), _bytes(key), out score);
+		}
+
+		public void zdel(byte[] name, byte[] key) {
+			List<byte[]> resp = request("zdel", name, key);
+			resp_code = _string(resp[0]);
+			this.assert_ok();
+		}
+
+		public void zdel(string name, string key) {
+			this.zdel(_bytes(name), _bytes(key));
+		}
+
+		public Int64 zsize(byte[] name) {
+			List<byte[]> resp = request("zsize", name);
+			resp_code = _string(resp[0]);
+			this.assert_ok();
+			if(resp.Count != 2) {
+				throw new Exception("Bad response!");
+			}
+			return Int64.Parse(_string(resp[1]));
+		}
+
+		public Int64 zsize(string name) {
+			return this.zsize(_bytes(name));
+		}
+
+		public KeyValuePair<string, Int64>[] zscan(string name, string key_start, Int64 score_start, Int64 score_end, Int64 limit) {
+			string score_s = "";
+			string score_e = "";
+			if(score_start != Int64.MinValue) {
+				score_s = score_start.ToString();
+			}
+			if(score_end != Int64.MaxValue) {
+				score_e = score_end.ToString();
+			}
+			List<byte[]> resp = request("zscan", name, key_start, score_s, score_e, limit.ToString());
+			KeyValuePair<string, byte[]>[] kvs = parse_scan_resp(resp);
+			KeyValuePair<string, Int64>[] ret = new KeyValuePair<string, Int64>[kvs.Length];
+			for(int i = 0; i < kvs.Length; i++) {
+				string key = kvs[i].Key;
+				Int64 score = Int64.Parse(_string(kvs[i].Value));
+				ret[i] = new KeyValuePair<string, Int64>(key, score);
+			}
+			return ret;
+		}
+
+		public KeyValuePair<string, Int64>[] zrscan(string name, string key_start, Int64 score_start, Int64 score_end, Int64 limit) {
+			string score_s = "";
+			string score_e = "";
+			if(score_start != Int64.MaxValue) {
+				score_s = score_start.ToString();
+			}
+			if(score_end != Int64.MinValue) {
+				score_e = score_end.ToString();
+			}
+			List<byte[]> resp = request("zrscan", name, key_start, score_s, score_e, limit.ToString());
+			KeyValuePair<string, byte[]>[] kvs = parse_scan_resp(resp);
+			KeyValuePair<string, Int64>[] ret = new KeyValuePair<string, Int64>[kvs.Length];
+			for(int i = 0; i < kvs.Length; i++) {
+				string key = kvs[i].Key;
+				Int64 score = Int64.Parse(_string(kvs[i].Value));
+				ret[i] = new KeyValuePair<string, Int64>(key, score);
+			}
+			return ret;
+		}
+	}
+}
---
+++ api/dotnet/Link.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace ssdb
+{
+	class Link
+	{
+		private TcpClient sock;
+		private MemoryStream recv_buf = new MemoryStream(8 * 1024);
+
+		public Link(string host, int port) {
+			sock = new TcpClient(host, port);
+			sock.NoDelay = true;
+			sock.Client.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.KeepAlive, true);
+		}
+
+		~Link() {
+			this.close();
+		}
+
+		public void close() {
+			if(sock != null){
+				sock.Close();
+			}
+			sock = null;
+		}
+
+		public List<byte[]> request(string cmd, params string[] args) {
+			List<byte[]> req = new List<byte[]>(1 + args.Length);
+			req.Add(Encoding.Default.GetBytes(cmd));
+			foreach(string s in args) {
+				req.Add(Encoding.Default.GetBytes(s));
+			}
+			return this.request(req);
+		}
+
+		public List<byte[]> request(string cmd, params byte[][] args) {
+			List<byte[]> req = new List<byte[]>(1 + args.Length);
+			req.Add(Encoding.Default.GetBytes(cmd));
+			req.AddRange(args);
+			return this.request(req);
+		}
+
+		public List<byte[]> request(List<byte[]> req) {
+			MemoryStream buf = new MemoryStream();
+			foreach(byte[] p in req) {
+				byte[] len = Encoding.Default.GetBytes(p.Length.ToString());
+				buf.Write(len, 0, len.Length);
+				buf.WriteByte((byte)'\n');
+				buf.Write(p, 0, p.Length);
+				buf.WriteByte((byte)'\n');
+			}
+			buf.WriteByte((byte)'\n');
+
+			byte[] bs = buf.GetBuffer();
+			sock.GetStream().Write(bs, 0, (int)buf.Length);
+			//Console.Write(Encoding.Default.GetString(bs, 0, (int)buf.Length));
+			return recv();
+		}
+
+		private List<byte[]> recv() {
+			while(true) {
+				List<byte[]> ret = parse();
+				if(ret != null) {
+					return ret;
+				}
+				byte[] bs = new byte[8192];
+				int len = sock.GetStream().Read(bs, 0, bs.Length);
+				//Console.WriteLine("<< " + Encoding.Default.GetString(bs));
+				recv_buf.Write(bs, 0, len);
+			}
+		}
+
+		private static int memchr(byte[] bs, byte b, int offset) {
+			for(int i = offset; i < bs.Length; i++) {
+				if(bs[i] == b) {
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		private List<byte[]> parse() {
+			List<byte[]> list = new List<byte[]>();
+			byte[] buf = recv_buf.GetBuffer();
+
+			int idx = 0;
+			while(true) {
+				int pos = memchr(buf, (byte)'\n', idx);
+				//System.out.println("pos: " + pos + " idx: " + idx);
+				if(pos == -1) {
+					break;
+				}
+				if(pos == idx || (pos == idx + 1 && buf[idx] == '\r')) {
+					idx += 1; // if '\r', next time will skip '\n'
+					// ignore empty leading lines
+					if(list.Count == 0) {
+						continue;
+					} else {
+						int left = (int)recv_buf.Length - idx;
+						recv_buf = new MemoryStream(8192);
+						if(left > 0) {
+							recv_buf.Write(buf, idx, left);
+						}
+						return list;
+					}
+				}
+				byte[] lens = new byte[pos - idx];
+				Array.Copy(buf, idx, lens, 0, lens.Length);
+				int len = Int32.Parse(Encoding.Default.GetString(lens));
+
+				idx = pos + 1;
+				if(idx + len >= recv_buf.Length) {
+					break;
+				}
+				byte[] data = new byte[len];
+				Array.Copy(buf, idx, data, 0, (int)data.Length);
+
+				//Console.WriteLine("len: " + len + " data: " + Encoding.Default.GetString(data));
+				idx += len + 1; // skip '\n'
+				list.Add(data);
+			}
+			return null;
+		}
+	}
+}
