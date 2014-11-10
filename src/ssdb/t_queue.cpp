637c93c1fdf3ebe62e47b023345420dd8fe2eb0f
ideawu <ideawu@local>
2014-11-10 10:15:50 +0800

    add copyright msg

--- src/ssdb/t_queue.cpp
+++ src/ssdb/t_queue.cpp
@@ -1,3 +1,8 @@
+/*
+Copyright (c) 2012-2014 The SSDB Authors. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be
+found in the LICENSE file.
+*/
 #include "t_queue.h"
 
 static int qget_by_seq(leveldb::DB* db, const Bytes &name, uint64_t seq, std::string *val){
--- src/util/sorted_set.h
+++ src/util/sorted_set.h
@@ -1,3 +1,8 @@
+/*
+Copyright (c) 2012-2014 The SSDB Authors. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be
+found in the LICENSE file.
+*/
 #ifndef UTIL_SORTED_SET_H
 #define UTIL_SORTED_SET_H
 
